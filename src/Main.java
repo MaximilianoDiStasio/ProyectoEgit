a23fbd43817d05074c55d755e4c9bd1be1a2ef8f
Maximiliano <mdispin671@g.educaand.es>
2022-12-12 16:31:18 +0100

    Principal2.2

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		int numero1, numero2, resultado;
 		System.out.print("Introduce primer número: ");
 		numero1 = sc.nextInt();
 		System.out.print("Introduce segundo número: ");
-		numero2 = sc.nextInt();}
-		
-		public static int suma(int a, int b) {
+		numero2 = sc.nextInt();
+	}
+
+	public static int suma(int a, int b) {
 			int c;
 			c = a + b;
 			return c;
@@ -24,13 +25,20 @@ public class Main {
 				c = a - b;
 				return c;
 		}
+		
 		public static int producto(int a, int b) {
 					int c;
 					c = a * b;
 					return c;
-					
-		
+		}
 
-	}
+		public static int dividir(int a, int b) {
+						int c;
+						c = a / b;
+						return c;
+		}
+
+					
+}
 
 }
