3c920401fb11a48ec3cea9e38826d203984de07a
Maximiliano <mdispin671@g.educaand.es>
2022-12-12 16:23:51 +0100

    Rama 1.1

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		int numero1, numero2, resultado;
 		System.out.print("Introduce primer número: ");
 		numero1 = sc.nextInt();
 		System.out.print("Introduce segundo número: ");
 		numero2 = sc.nextInt();}
-
+		
 		public static int suma(int a, int b) {
 			int c;
 			c = a + b;
 			return c;
 			
-
+			resultado = suma(numero1, numero2);
+			System.out.println("Suma: " + resultado);
 
 	}
 
