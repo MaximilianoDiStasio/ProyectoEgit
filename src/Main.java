7eebc378c8f90c8f205b0451caceb45701384bf1
Maximiliano <mdispin671@g.educaand.es>
2022-12-12 16:29:04 +0100

    Principal2.0 y 2.1

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,18 @@ public class Main {
 			
 			resultado = suma(numero1, numero2);
 			System.out.println("Suma: " + resultado);
+			
+		public static int resta(int a, int b) {
+				int c;
+				c = a - b;
+				return c;
+		}
+		public static int producto(int a, int b) {
+					int c;
+					c = a * b;
+					return c;
+					
+		
 
 	}
 
